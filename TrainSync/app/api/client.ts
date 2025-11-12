406d01bb4e92bd7f2f258a187046c69768b2b0fe
sajal gupta <sajal147x@gmail.com>
2025-11-12 15:47:12 -0600

    config

--- TrainSync/app/api/client.ts
+++ TrainSync/app/api/client.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 // Use your computer's IP if running on Expo mobile
-const API_BASE = "http://localhost:8080/api";
+// To find your IP: run `ifconfig | grep "inet " | grep -v 127.0.0.1` on Mac/Linux
+// or `ipconfig` on Windows and look for IPv4 Address
+const API_BASE = "http://10.0.0.199:8080/api";
 
 const client = axios.create({
   baseURL: API_BASE,
@@ -13,7 +15,7 @@ client.interceptors.request.use(async (config) => {
   try {
     // Ensure headers object exists
     if (!config.headers) {
-      config.headers = {};
+      config.headers = {} as any;
     }
     
     const stored = await SecureStore.getItemAsync("jwt");
